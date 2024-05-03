c1226e2171de3f3ee1246ae0c13bee2f78875985
Manpasu <manpasu@protonmail.com>
2024-05-03 03:08:33 +0100

    Hello world program

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Learning_CPP
-Learning C++
+# This repo is for Learning CPP
+Below are the list projects I built while learning C++
+
+## Hello World
+This is a simple **Hello world** program. 
+There is nothing special about it.
+
---
+++ hello.cpp
@@ -0,0 +1,15 @@
+/****************************************
+ * This is a hello world program
+*/
+using namespace std;
+#include <iostream>
+
+
+
+int main()
+    {
+        string name{"Manpasu"};
+        std::cout<< "Hello " << name <<endl;
+
+
+    }
